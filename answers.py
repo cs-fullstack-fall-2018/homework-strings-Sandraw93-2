fead93aa8fd2ce7cd2bbd96d7d57676e675ee644
sandra williams <sannwilliams93@yahoo.com>
2018-07-12 09:29:04 -0500

    check in

---
+++ answers.py
@@ -0,0 +1,6 @@
+(x) = (sann)
+print (x)
+#The error message returned when you improperly use quotes inside of strings in this example is name error. The name sann is not defined.
+
+x = [input('apples')] , [input('bananas')] , [input('carrots')]
+print ('x')
